427a50cba882120c793b28e5842f2463929a9c08
Ahmad Horyzat <171059@ppu.edu.ps>
2021-01-21 07:24:58 +0200

    add engines to package.json

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/Radio'));
+app.use(express.static(__dirname + '/dist/radio'));
 
 app.all('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/Radio/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/radio/index.html'));
 });
 
 const port = process.env.PORT
